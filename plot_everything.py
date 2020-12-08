57f2bce4dc9e0f94dda5e59f4788389a5ae1d220
Simon Demeule <simondemeule@gmail.com>
2020-12-07 22:03:43 -0500

    Restructured project to preserve training runs

--- plot_everything.py
+++ plot_everything.py
@@ -1,12 +1,26 @@
-import plotting as p
+from utils import find_last_run_index
+from plotting import *
 
-datasets = ['MNIST', 'svhn', 'CIFAR', 'Gaussian']
-divergence_names = ['total_variation', 'forward_kl', 'reverse_kl', 'pearson', 'hellinger', 'jensen_shannon']
+datasets_all = ['MNIST', 'SVHN', 'CIFAR', 'Gaussian']
+divergences_all = ['total_variation', 'forward_kl', 'reverse_kl', 'pearson', 'hellinger', 'jensen_shannon']
 
-for dataset in datasets:
-    p.plot_divergence_all(dataset, show_plot=False)
-    p.plot_real_fake_all(dataset, show_plot=False)
-    for divergence in divergence_names:
-        p.plot_divergence_training(dataset, divergence, show_plot=False)
-        p.plot_divergence_other(dataset, divergence, show_plot=False)
-        p.plot_real_fake_training(dataset, divergence, show_plot=False)
+for dataset in datasets_all:
+    # Plots that comprise of all divergences for a dataset
+    runs = []
+    divergences = []
+    for divergence in divergences_all:
+        run = find_last_run_index(dataset, divergence)
+        if run != -1:
+            # There exists at least a run for this divergence
+            # WARNING: this will not plot the latest run if the increasing numbering sequence is broken. 
+            # It will start at zero and go up from there until no further run is found.
+            divergences.append(divergence)
+            runs.append(run)
+    plot_divergence_all(dataset, divergences, runs, show_plot=False)
+    plot_real_fake_all(dataset, divergences, runs, show_plot=False)
+
+    # Plots that are specific to each divergence
+    for (divergence, run) in zip(divergences, runs):
+        plot_divergence_training(dataset, divergence, run, show_plot=False)
+        plot_divergence_other(dataset, divergence, run, show_plot=False)
+        plot_real_fake_training(dataset, divergence, run, show_plot=False)
